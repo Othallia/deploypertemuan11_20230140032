1540cb85af7a76c3eb700b131b681edddb30b7ca
Othallia <vianiputriocta6@gmail.com>
2026-05-19 14:21:49 +0700

    add ProfileRepository

--- src/main/java/com/deploy/pertemuan11/repository/ProfileRepository.java
+++ src/main/java/com/deploy/pertemuan11/repository/ProfileRepository.java
@@ -1,4 +1,11 @@
 package com.deploy.pertemuan11.repository;
 
-public interface ProfileRepository {
+
+import com.deploy.pertemuan11.model.profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface ProfileRepository extends JpaRepository<profile, String> {
 }
